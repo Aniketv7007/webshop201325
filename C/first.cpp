4b65bb15e9d48ef3d9a416121c333ba9a8f37479
Aniketv7007 <aniketannu1122@gmail.com>
2021-12-03 17:55:39 +0530

    new file added

---
+++ C/first.cpp
@@ -0,0 +1,119 @@
+// #include<iostream>
+// using namespace std;
+
+// int glo = 6;
+// void sum ()
+// {
+//     int a;
+//     cout<< glo;
+// }
+//     int main ()
+//     {
+//         int glo = 9;
+//         glo =78;
+//         int a= 4;
+//         int b = 5;
+//         sum ();
+//         cout<<glo;
+
+//     }
+
+// #include<iostream>
+// using namespace std ;
+// int main ()
+// {
+//     int num1,num2;
+//     cout<<"Enter the value of num 1:\n";
+//     cin>>num1;
+
+//     cout<<"Enter the value of num 2:\n";
+//     cin>>num2;
+    
+//     cout<< "The sum is "<<num1+num2;
+//     return 0;
+// }
+
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     int a=5,b=6;
+//     cout<<"operator in C++"<<endl;
+//     cout<<"following are the type of operator in c++"<<endl;
+//     // Arithmetic operator 
+//     cout<<"The value of a + b is "<<a+b<<endl;
+//     cout<<"The value of a - b is "<<a-b<<endl;
+//     cout<<"The value of a * b is "<<a*b<<endl;
+//     cout<<"The value of a / b is "<<a/b<<endl;
+//     cout<<"The value of a % b is "<<a%b<<endl;
+    
+
+//     return 0;
+// } 
+
+// #include<iostream>
+// using namespace std;
+// int main ()
+// {
+//     int a=4,b=6;
+//     //comaparison operator
+//     cout<<"The value of a == b is "<<(a==b)<<endl;
+
+//     cout<<"The value of a != b is "<<(a!=b)<<endl;
+
+//     cout<<"The value of a <= b is "<<(a<=b)<<endl;
+
+//     cout<<"The value of a >= b is "<<(a>=b)<<endl;
+
+//     cout<<"The value of a > b is "<<(a>b)<<endl;
+
+//     cout<<"The value of a < b is "<<(a<b)<<endl;
+//     return 0;
+// }
+
+// #include<iostream>
+// using namespace std;
+// int c = 45;
+// int main ()
+// {
+//     int a,b,c;
+//     float d =34.4;
+//     long double e = 34.4;
+//     cout<<"Enter the value of a:"<<endl;
+//     cin>>a;
+//     cout<<"Enter the value of b:"<<endl;
+//     cin>>b;
+//     c = a + b
+//     cout<<"The sum is "<<c<<endl;
+//     cout<<"The global c is "<<::c;
+//     cout<<"The value of d is "<<d<<endl<<"The value of e is"<<e;
+
+//     return 0;
+
+
+// }
+
+// #include<iostream>
+// using namespace std;
+// int main ()
+// {
+//     float d = 34.4F;
+//     long double e = 34.4L;
+//     cout<<"The value of 34.4 is "<<sizeof(34.4)<<endl;
+//     cout<<"The value of 34.4f is "<<sizeof(34.4f)<<endl;
+//     cout<<"The value of 34.4F is "<<sizeof(34.4F)<<endl;
+//     cout<<"The value of 34.4L is "<<sizeof(34.4L)<<endl;
+//     cout<<"The value of 34.4l is "<<sizeof(34.4l)<<endl;
+//     return 0;
+
+// }
+//*********************** Reference variable **************************
+
+#include<iostream>
+
+using namespace std;
+
+int main ()
+{
+
+}
---
+++ code/first_problem.c
@@ -0,0 +1,33 @@
+/*#include<stdio.h>
+int main()
+{
+    int a = 4;
+    float b = 2.2;
+    char c =  'A';
+    int d = 6;
+    printf("the sum of a and d is %d\n",a+d);
+    printf("the value of a is %d\n",a);
+    printf("the value of a is %f\n",b);
+    printf("the value of a is %c\n",c); 
+    return 0;
+}*/
+/*#include<stdio.h>
+int main ()
+ { 
+     int a = 10;
+     int b = 20;
+     printf("the sum a and b is  %d",a+b);
+    return 0;
+}*/
+#include<stdio.h>
+int main ()
+{
+    int a,b;
+    printf("Enter your first number a :");
+    scanf("%d",&a);
+    printf("Enter your second number b :");
+    scanf("%d",&b);
+    printf("The sum of a and b is %d:",a+b);
+    return 0;
+
+}
